bbb2d1567cf387464d6e130afff038cbaad013a7
Krasimir <krasimir99@gmail.com>
2016-02-08 12:10:38 +0200

    test
    this my first page

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link href="style/stilove.css" rel="stylesheet" type="text/css" />
+        <title></title>
+    </head>
+    <body>
+        <div id="container">
+            
+            <div id="head">
+                <div class="logo"></div>
+            </div>
+            <div id="menu">
+                <div class="horizontal">
+                    <ul>
+                        <li><a href="index.php" title="Начало" target="_self">Начало</a></li>
+                        <li><a href="index.php" title="За Нас" target="_blank">За Нас</a></li>
+                        <li><a href="index.php" title="Продукти" target="_blank">Продукти</a></li>
+                        <li><a style="border-right: none" href="index.php" title="Контакти" target="_self">Контакти</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div id="body">
+                <div class="vertmenu">
+                     <div class="vertical">
+                    <ul>
+                        <li><a href="index.php" title="Начало" target="_self">Начало</a></li>
+                        <li><a href="index.php" title="За Нас" target="_blank">За Нас</a></li>
+                        <li><a href="index.php" title="Продукти" target="_blank">Продукти</a></li>
+                        <li><a style="border-right: none" href="index.php" title="Контакти" target="_self">Контакти</a></li>
+                    </ul>
+                </div>
+                </div>
+                <div class="content">
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                      <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                       <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, 
+                    sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                  
+                </div>
+            </div>           
+        </div>
+        <div style="clear: both;"></div>
+        <div id="footer">footer</div>
+    </body>
+</html>
